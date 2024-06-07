dcc61afb69c6eb8539c487b588d196a4f4777732
krish-odhaviya <krish.odhaviya12@gmail.com>
2024-06-07 14:07:42 +0530

    chages in index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use("*", cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
